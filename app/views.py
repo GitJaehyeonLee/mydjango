a6f9bb33e4db285d830381b9cef6c0997c5936c7
jaehyeonlee <g.jaehyeonlee@gmail.com>
2022-09-18 19:59:17 +0900

    added app-> post_list view

--- app/views.py
+++ app/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
 
+from app.models import Post
+
+
 # Create your views here.
+def index(request: HttpRequest) -> HttpResponse:
+    # qs = [
+    #     {"id": 1, "title": "post #1"},
+    #     {"id": 2, "title": "post #2"},
+    # ]
+    qs = Post.objects.all()
+    return render(request, "app/index.html", {"post_list": qs})
--- mydjango/urls.py
+++ mydjango/urls.py
@@ -16,6 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+import app.views
+
+urlpatterns = [path("admin/", admin.site.urls), path("app/", app.views.index)]
