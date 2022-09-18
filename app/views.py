8b31ba10356740cfe3026f4b228b80416d2c9350
jaehyeonlee <g.jaehyeonlee@gmail.com>
2022-09-18 20:11:07 +0900

    added app-> post_detail view

--- app/views.py
+++ app/views.py
@@ -1,14 +1,19 @@
 from django.http import HttpResponse, HttpRequest
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from app.models import Post
 
 
 # Create your views here.
 def index(request: HttpRequest) -> HttpResponse:
-    # qs = [
-    #     {"id": 1, "title": "post #1"},
-    #     {"id": 2, "title": "post #2"},
-    # ]
     qs = Post.objects.all()
     return render(request, "app/index.html", {"post_list": qs})
+
+
+def post_detail(request: HttpRequest, pk: int) -> HttpResponse:
+    post = get_object_or_404(Post, pk=pk)
+    return render(
+        request,
+        "app/post_detail.html",
+        {"post": post},
+    )
