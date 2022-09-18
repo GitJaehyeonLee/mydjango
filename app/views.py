caaf11cfa1b0d0619241e66e4c04f36cb9e73bdd
jaehyeonlee <g.jaehyeonlee@gmail.com>
2022-09-18 22:16:43 +0900

    added - django Bootstrap 5 적용 및 Post new 페이지 추가

--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import CreateView
 
+from app.forms import PostForm
 from app.models import Post
 
 
@@ -17,3 +19,6 @@ def post_detail(request: HttpRequest, pk: int) -> HttpResponse:
         "app/post_detail.html",
         {"post": post},
     )
+
+
+post_new = CreateView.as_view(model=Post, form_class=PostForm, success_url="/app/")
